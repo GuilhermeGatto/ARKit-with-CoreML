29d8041a68515ed6a65d29923c559f38faca058b
Guilherme Gatto <guuilhermegatto@hotmail.com>
2018-03-02 14:31:17 -0300

    arkit coreml vision

---
+++ ARKit-Vision/ARKit-Vision/ARBridge.swift
@@ -0,0 +1,19 @@
+//
+//  ARBridge.swift
+//  ARKit-Vision
+//
+//  Created by Guilherme Gatto on 28/02/18.
+//  Copyright © 2018 Guilherme Gatto. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import ARKit
+
+class ARBridge {
+    
+    static let shared = ARBridge()
+    
+    var anchorsToIdentifiers = [ARAnchor : String]()
+    
+}
